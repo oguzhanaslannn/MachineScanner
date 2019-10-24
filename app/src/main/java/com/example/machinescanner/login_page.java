f7f2671ef2adfc118a7f9b96d877c5da98fb2cbd
Bahadır Enes Atay <>
2019-10-24 17:22:40 +0300

    Some pages are designed.

---
+++ app/src/main/java/com/example/machinescanner/login_page.java
@@ -0,0 +1,13 @@
+package com.example.machinescanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
